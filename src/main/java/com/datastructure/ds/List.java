2649e4d9d2b7688c299658829bed6fb688607a54
王天宇 <tianyu.wang@moji.com>
2020-08-14 09:51:52 +0800

    add

--- src/main/java/com/datastructure/ds/List.java
+++ src/main/java/com/datastructure/ds/List.java
@@ -5,7 +5,9 @@ public interface List<E>{
     //返回线性表的大小
     public int getSize();
 
+    //判断线性表中是否为空
     public boolean isEmpty();
 
-    public boolean contains(E);
+    //判断线性表中是否包含元素o
+    public boolean contains(E o);
 }
