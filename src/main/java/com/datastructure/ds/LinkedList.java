30a89eedcda46c6969a130f38ff54afe26b986d7
王天宇 <houziwty@126.com>
2020-10-09 15:36:34 +0800

    linklist

---
+++ src/main/java/com/datastructure/ds/LinkedList.java
@@ -0,0 +1,43 @@
+package com.datastructure.ds;
+
+public class LinkedList<E> implements  List<E> {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E o) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(E e) {
+        return 0;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, E e) {
+
+    }
+
+    @Override
+    public void add(int index, E e) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+}
