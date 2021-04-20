73689529629208a81c7d86766dddec6515703f37
王天宇 <houziwty@126.com>
2021-04-20 21:09:10 +0800

    Update BinarySeachTree.java

--- src/main/java/com/datastructure/ds/tree/BinarySeachTree.java
+++ src/main/java/com/datastructure/ds/tree/BinarySeachTree.java
@@ -6,9 +6,9 @@ package com.datastructure.ds.tree;
  * @description:二查搜索树
  */
 public class BinarySeachTree {
-    private int color=0;//0表示黑，1表示红
+    private int color = 0;//0表示黑，1表示红
 
-    int data ;
+    int data;
 
     BinarySeachTree left;
 
@@ -16,7 +16,7 @@ public class BinarySeachTree {
 
     BinarySeachTree parent;
 
-    public  BinarySeachTree(int data){
+    public BinarySeachTree(int data) {
         this.data = data;
         this.left = null;
         this.parent = null;
@@ -25,5 +25,53 @@ public class BinarySeachTree {
         //parent.parent	;爷爷
         //parent.parent.left 左边的叔叔
         //parent.left 兄弟姐妹
+    }
+
+    //插入的时候每次都是和根结点比较。一直要找到它应该插入的位置。
+    //肯定会插在叶子结点。那么其实大家可以看到 插入其实就是查找。 默认root不会为空
+
+     void insert(BinarySeachTree root, int data) {
+        //if(root == null) {}
+        if (root.data < data) {//根节点小 我们要放到右边
+            if (root.right == null) {
+                root.right = new BinarySeachTree(data);
+            } else {
+                insert(root.right, data);
+            }
+        } else {
+            if (root.left == null) {
+                root.left = new BinarySeachTree(data);
+            } else {
+                insert(root.left, data);
+            }
+        }
+    }
+
+      void find(BinarySeachTree root, int data) {
+        if (root != null) {
+            if (root.data < data) {
+                find(root.right, data);
+            } else if (root.data > data) {
+                find(root.left, data);
+            }else {
+                System.out.println("找到了");
+                System.out.println(root.data);
+                return ;
+            }
+        }
+    }
+     void pre(){//前序遍历 根(输出) 左 右
+
+    }
+      void post(){//后序遍历  左  右 根(输出)
+
+    }
+      void in(BinarySeachTree root){//中序遍历  左 根(输出)  右
+        if(root!=null){
+            in(root.left);
+            System.out.println(root.data);
+            in(root.right);
+        }
+
     }
 }
