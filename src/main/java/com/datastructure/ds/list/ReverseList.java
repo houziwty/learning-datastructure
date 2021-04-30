1df044fec1fb6790c20ad8a76955b58946b7d6b5
王天宇 <houziwty@126.com>
2021-04-30 22:26:31 +0800

    Update ReverseList.java

--- src/main/java/com/datastructure/ds/list/ReverseList.java
+++ src/main/java/com/datastructure/ds/list/ReverseList.java
@@ -10,4 +10,58 @@ package com.datastructure.ds.list;
  * 输入: 1->2->3->4->5 输出: 5->4->3->2->1
  */
 public class ReverseList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //迭代方法
+    public static ListNode iterate(ListNode head) {
+        ListNode prev = null, curr, next;//定义前节点 当前  下一个
+        curr = head;//当前节点等于head
+        while (curr != null) {//循环到等于空节点就是到头部了
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static ListNode recursion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = recursion(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode node = iterate(node1);
+        ListNode nodeRecurs = recursion(node1);
+        System.out.println(nodeRecurs);
+
+        ListNode nodeShow=node;
+
+        for(int i=0;i<5;i++){
+            System.out.println(nodeShow.val);
+            nodeShow=nodeShow.next;
+        }
+
+    }
+
+
 }
