0adde1c0be1316a7ba721784bf61620857248283
王天宇 <houziwty@126.com>
2021-05-06 10:35:23 +0800

    Update LinkedQueue.java

--- src/main/java/com/datastructure/ds/queue/LinkedQueue.java
+++ src/main/java/com/datastructure/ds/queue/LinkedQueue.java
@@ -3,20 +3,24 @@ package com.datastructure.ds.queue;
 /**
  * @author : tianyu.wang
  * create at:  2021/3/1  5:18 下午
- * @description:
+ * @description:单项队列
  */
 public class LinkedQueue<E> implements Queue<E> {
 
-    private static class Node<E>{
+    //构建node
+    private static class Node<E> {
         private E data;
+        //下一个链表
         private Node<E> next;
-        public Node(E data,Node<E>next){
-            this.data=data;
-            this.next=next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
         }
     }
-    private  Node<E> head;//队头指针
-    private Node<E>tail;//队尾指针
+
+    private Node<E> head;//队头指针
+    private Node<E> tail;//队尾指针
 
     private int size;//队列元素数量
 
@@ -27,19 +31,20 @@ public class LinkedQueue<E> implements Queue<E> {
 
     @Override
     public boolean empty() {
-        return size==0;
+        return size == 0;
     }
 
     @Override
     public void enqueue(E e) {
-        Node<E>prev=tail;
+        Node<E> prev = tail;
         //赋值进入队尾
-        tail=new Node<>(e,null);
-
-        if(size==0)
-            head=tail;
+        tail = new Node<>(e, null);
+        //如果为空 头部等于队尾 否则下一个节点指向队尾
+        if (size == 0)
+            head = tail;
         else
-            prev.next=tail;
+            prev.next = tail;
+        //队列size++
         size++;
 
     }
@@ -47,20 +52,24 @@ public class LinkedQueue<E> implements Queue<E> {
     @Override
     public E dequeue() {
         //出队列
-        if(size==0){
+        if (size == 0) {
             throw new RuntimeException("队列为空...");
         }
-        E result=head.data;
-        head=head.next;
+        //结果赋值为头部
+        E result = head.data;
+        //头部赋值下一个指针
+        head = head.next;
+        //size--
         size--;
-        if(size==0)
-            tail=null;
+        //如果等于空 尾部为空
+        if (size == 0)
+            tail = null;
         return result;
     }
 
     @Override
     public E peek() {
-        if(size==0){
+        if (size == 0) {
             throw new RuntimeException("队列为空...");
         }
         return head.data;
