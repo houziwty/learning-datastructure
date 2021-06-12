1c6783a0b7c46f2bb45dfabfa170cb14001ea24a
王天宇 <houziwty@126.com>
2021-06-12 15:56:21 +0800

    Update MergeTwoLists.java

--- src/main/java/com/datastructure/ds/leetcode/MergeTwoLists.java
+++ src/main/java/com/datastructure/ds/leetcode/MergeTwoLists.java
@@ -70,6 +70,21 @@ public class MergeTwoLists {
         return new String(res);
     }
 
+    //股票最大利益
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+
 
 
 }
